77e1cc09251087d57ce41e40d91d2336bd17080c
kobi666 <kobi.malul@gmail.com>
2019-07-03 00:34:15 +0300

    removed unneccesery shit

--- TDmayhem/Assets/Scripts/test.cs
+++ TDmayhem/Assets/Scripts/test.cs
@@ -2,50 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : StateMachine
+public class test : MonoBehaviour
 {
   
-                private BezierSolution.EnemyWalker2D WalkerOBJ;
-                string[] stateStrings = {"Walking", "WaitForBattle", "Dead", "InBattle", "Spawning", "Shooting"};
-                
-                public GameUnit gameUnit;
-                
-                
-                
-                
-
-
-
-                private void Awake() 
-                {
-                    WalkerOBJ = gameObject.GetComponent<BezierSolution.EnemyWalker2D>();
-                }
-                
-
-
-
-                void HorizontalFlipper(bool HorizontalDirection)
-                {
-                    if (HorizontalDirection == true)
-                    {
-                        GetComponent<SpriteRenderer>().flipX = true;
-                    }
-                    else
-                    {
-                        GetComponent<SpriteRenderer>().flipX = false;
-                    }
-                }
-                // Start is called before the first frame update
-                void Start()
-                {
-                    gameUnit = gameObject.GetComponent<GameUnit>();
-
-                    
-                }
-
-                // Update is called once per frame
-                void Update()
-                {
-                    HorizontalFlipper(gameObject.GetComponent<BezierSolution.EnemyWalker2D>().Hdirection);
-                }
 }
